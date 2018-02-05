30254859453351e4df3954cb5a02c234ac3f7697
胡小忠 <1466947023@qq.com>
2018-02-05 21:04:39 +0800

    提交本地readme

---
+++ src/com/mrhu/servletlearning/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.mrhu.servletlearning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("pdf");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>HelloWorld</h1>");
+		out.close();
+	}
+
+}
